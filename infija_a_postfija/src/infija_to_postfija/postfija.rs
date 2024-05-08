e59944de1d14ccc77d8c3a40db95aa3dc950d006
Luis Calle <luedu1103@gmail.com>
2024-05-07 23:18:18 -0500

    Orden: A medias xddddd

--- infija_a_postfija/src/infija_to_postfija/postfija.rs
+++ infija_a_postfija/src/infija_to_postfija/postfija.rs
@@ -1,7 +1,43 @@
 pub mod postfija_and_popall{
     use std::collections::HashMap;
 
-    pub fn postfija(map: &HashMap<String, u32> ,pila: &mut Vec<String>, i: &char, mut operation_string: String, postfija_mostrar: &mut Vec<String>){
+    pub fn postfija(string: &String, map: &HashMap<String, u32>) -> Vec<String>{
+        let mut postfija_mostrar: Vec<String> = Vec::new();
+        let mut pila: Vec<String> = Vec::new();
+        let mut datos: Vec<char> = Vec::new();
+        
+        for i in string.chars() {
+            datos.push(i);
+        }
+
+        for i in &datos {
+            match map.get(&i.to_string()) {
+                Some(_value) => {
+                    postfija_rules(&map, &mut pila, &i,String::from(""), &mut postfija_mostrar);
+                },
+                None => {
+                    let ultimo = postfija_mostrar.last().cloned();
+                    if let Some(mut last_string) = ultimo {
+                        if let Some(character) = last_string.chars().next() {
+                            if character.is_digit(10) {
+                                last_string += &i.to_string();
+                                postfija_mostrar.pop();
+                                postfija_mostrar.push(last_string);
+                                continue;
+                            }
+                        }
+                    }
+                    postfija_mostrar.push(i.to_string());
+                },
+            }
+        }
+
+        pop_everything(&mut pila, &mut postfija_mostrar);
+        postfija_mostrar.retain(|s| !s.is_empty());
+        postfija_mostrar
+    }
+
+    pub fn postfija_rules(map: &HashMap<String, u32> ,pila: &mut Vec<String>, i: &char, mut operation_string: String, postfija_mostrar: &mut Vec<String>){
         // Si la pila esta vacia, se le coloca el operador
         if pila.is_empty() {
             pila.push(i.to_string());
--- infija_a_postfija/src/main.rs
+++ infija_a_postfija/src/main.rs
@@ -14,60 +14,15 @@ fn main() {
     map.insert("(".to_string(), 0);
     map.insert(")".to_string(), 0);
 
-    let mut pila: Vec<String> = Vec::new();
-    let mut datos: Vec<char> = Vec::new();
-    let mut postfija_mostrar: Vec<String> = Vec::new();
-    // let mut operationString: Vec<String> = Vec::new();
-
-    // let string: String = String::from("4*(5+6-(8/2^3)-7)-1");
+    let string: String = String::from("4*(5+6-(8/2^3)-7)-1");
     // let string: String = String::from("29-9*2^10");
-    let string: String = String::from("29^9*2-10");
+    // let string: String = String::from("29^9*2-10");
     // let string: String = String::from("10+5.2-200");
     // let string: String = String::from("3*11+2004+1+2000+3");
-    // let mut string_nuevo: String = String::new();
-    for i in string.chars() {
-        datos.push(i);
-    }
 
-    for i in &datos {
-        match map.get(&i.to_string()) {
-            Some(_value) => {
-                postfija_and_popall::postfija(&map, &mut pila, &i,String::from(""), &mut postfija_mostrar);
-            },
-            None => {
-                let ultimo = postfija_mostrar.last().cloned();
-                if let Some(mut last_string) = ultimo {
-                    if let Some(character) = last_string.chars().next() {
-                        if character.is_digit(10) {
-                            last_string += &i.to_string();
-                            postfija_mostrar.pop();
-                            postfija_mostrar.push(last_string);
-                            continue;
-                        }
-                    }
-                }
-                postfija_mostrar.push(i.to_string());
-            },
-        }
-    }
-
-    // string_nuevo += &postfija_and_popall::pop_everything(&mut pila);
-    // println!("{:?}", pila);
-    postfija_and_popall::pop_everything(&mut pila, &mut postfija_mostrar);
-    postfija_mostrar.retain(|s| !s.is_empty());
-
-    for character in &postfija_mostrar {
-        match map.get(&character.to_string()) {
-            Some(_value) => {
-                evaluating::evaluate_post(&mut pila, &character);
-            },
-            None => {
-                pila.push(character.to_string());
-            },
-        }
-    }
+    let postfija_mostrar = postfija_and_popall::postfija(&string, &map);
 
     println!("{}", &string);
     println!("{:?}", &postfija_mostrar);
-    println!("{:?}", pila[0]);
+    evaluating::evaluate(&postfija_mostrar, &map);
 }
